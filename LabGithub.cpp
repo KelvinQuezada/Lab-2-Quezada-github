4627bc9b26999c8b020979957a6c89514cb083a2
KelvinQuezada <93734023+KelvinQuezada@users.noreply.github.com>
2021-11-09 17:38:14 -0500

    Create LabGithub.cpp

---
+++ LabGithub.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "================================="<<endl;
+    cout << "	Hola Bienvenidos Github" << endl;
+    cout << "================================="<<endl;
+    cout << "Trabajo de KELVIN QUEZADA "<<endl;
+    cout << "Segundo A "<<endl;
+    cout << "================================="<<endl;
+    return 0;
+}
