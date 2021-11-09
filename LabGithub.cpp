d5832369154f493e85dc96c293c0ed6322f18822
KelvinQuezada <93734023+KelvinQuezada@users.noreply.github.com>
2021-11-09 17:49:53 -0500

    Update LabGithub.cpp

--- LabGithub.cpp
+++ LabGithub.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
    cout << "================================="<<endl;
     cout << "	Hola Bienvenidos Github" << endl;
-    cout << "================================="<<endl;
+   cout << "09/11/2021"<<endl;
+   cout << "================================="<<endl;
     cout << "Trabajo de KELVIN QUEZADA "<<endl;
     cout << "Segundo A "<<endl;
     cout << "================================="<<endl;
